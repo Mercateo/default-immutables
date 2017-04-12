3e4e9076521df8db265eb1e80ae4843defd9aa54
Andreas Würl <andi@tryb.de>
2017-04-12 08:00:41 +0200

    updated versions

--- src/main/java/com/mercateo/immutables/DataClass.java
+++ src/main/java/com/mercateo/immutables/DataClass.java
@@ -8,8 +8,6 @@ import java.lang.annotation.Target;
 import org.immutables.value.Value;
 
 @Target({ ElementType.PACKAGE, ElementType.TYPE })
-@Retention(RetentionPolicy.CLASS) // Make it class retention for incremental
-                                  // compilation
 @Value.Style(init = "with*", visibility = Value.Style.ImplementationVisibility.PRIVATE, builderVisibility = Value.Style.BuilderVisibility.PUBLIC)
 public @interface DataClass {
 }
